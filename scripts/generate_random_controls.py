fb045b072196b99b47052fb38226dafc49a1b616
jackiedseal <jacquelinedseal@gmail.com>
2025-04-28 12:06:23 -0400

    Added updated umap generation and random sequence generation scripts.

--- scripts/generate_random_controls.py
+++ scripts/generate_random_controls.py
@@ -4,16 +4,21 @@ import sys
 from llfizz.constants import AMINOACIDS
 
 # COX15 is 45AAs
-# DED1 is 72AAs
+# DED1 CTD is 72AAs
+# DED1 NTD is 55AAs
+
 
 def generate_protein_sequence(length):
-    return "".join(random.choices(AMINOACIDS, k=length))
+    # return "M" + "".join(random.choices(AMINOACIDS, k=(length - 1)))
+    return "".join(random.choices(AMINOACIDS, k=(length)))
+
 
 def write_fasta_file(output_file, seq_length, num_sequences=100):
     with open(output_file, "w") as f:
         for i in range(num_sequences):
             sequence = generate_protein_sequence(seq_length)
             f.write(f">Sequence_{i+1}\n{sequence}\n")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
--- scripts/generate_umap.py
+++ scripts/generate_umap.py
@@ -6,54 +6,98 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 import plotly.express as px
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Perform UMAP on protein feature embeddings.")
-    parser.add_argument("input_file", type=str, help="Path to the CSV file containing protein feature embeddings.")
-    parser.add_argument("embedding", type=str, help="Type of embeddings to use (ie. one of 'original', 'LLPhyScore', 'hybrid').")
-    parser.add_argument("output_png", type=str, help="Path and name of the output PNG file for the plot.")
+    parser = argparse.ArgumentParser(
+        description="Perform UMAP on protein feature embeddings."
+    )
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Path to the CSV file containing protein feature embeddings.",
+    )
+    parser.add_argument(
+        "embedding",
+        type=str,
+        help="Type of embeddings to use (ie. one of 'original', 'LLPhyScore', 'hybrid').",
+    )
+    parser.add_argument(
+        "output_png",
+        type=str,
+        help="Path and name of the output PNG file for the plot.",
+    )
     return parser.parse_args()
 
+
 embedding_to_column = {
-    'original': (1, 127),
-    'LLPhyScore': (127, 143),
-    'hybrid': (1, 143)
+    "original": (1, 127),
+    "LLPhyScore": (127, 143),
+    "maxLLPhyScore": (1, 17),
+    "original+LLPhyScore": (1, 143),
+    "ESM2": (3, 1282),
 }
+
 
 def main():
     args = parse_arguments()
 
     df = pd.read_csv(args.input_file)
 
+    initial_shape = df.shape
+    df = df.dropna()
+    final_shape = df.shape
+    if initial_shape != final_shape:
+        print(
+            f"Dropped {initial_shape[0] - final_shape[0]} rows containing NaN values."
+        )
+
     column_range = embedding_to_column[args.embedding]
-    features = df.iloc[:, column_range[0]:column_range[1]]
+    features = df.iloc[:, column_range[0] : column_range[1]]
     print(features.columns)
 
     scaler = StandardScaler()
     features_scaled = scaler.fit_transform(features)
 
     # Apply UMAP to reduce the features to 2 dimensions
-    umap_model = umap.UMAP(n_components=2, random_state=42) 
+    umap_model = umap.UMAP(n_components=2, random_state=42)
     umap_embeddings = umap_model.fit_transform(features_scaled)
 
-    df['UMAP1'] = umap_embeddings[:, 0]
-    df['UMAP2'] = umap_embeddings[:, 1]
+    df["UMAP1"] = umap_embeddings[:, 0]
+    df["UMAP2"] = umap_embeddings[:, 1]
 
     sns.set(style="whitegrid")
 
     plt.figure(figsize=(10, 8))
-    sns.scatterplot(x='UMAP1', y='UMAP2', hue='localization', data=df, palette="Set1", s=20, marker="o", alpha=0.5)
+    sns.scatterplot(
+        x="UMAP1",
+        y="UMAP2",
+        hue="label",
+        data=df,
+        palette="Set1",
+        s=20,
+        marker="o",
+        alpha=0.5,
+    )
 
-    plt.title(f"UMAP projection of {args.embedding} features embedding of DeepLoc training sequences", fontsize=14)
+    plt.title(
+        f"UMAP projection of {args.embedding} features embedding of ProtGPS training sequences",
+        fontsize=14,
+    )
     plt.xlabel("UMAP1", fontsize=14)
     plt.ylabel("UMAP2", fontsize=14)
-    plt.legend(title='Localization', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.legend(title="Localization", bbox_to_anchor=(1.05, 1), loc="upper left")
 
-    plt.savefig(args.output_png, bbox_inches='tight')
+    plt.savefig(args.output_png, bbox_inches="tight")
 
-    fig = px.scatter(df, x='UMAP1', y='UMAP2', color='localization', 
-                     title=f"UMAP projection of {args.embedding} features embedding of DeepLoc training sequences",
-                     labels={'UMAP1': 'UMAP1', 'UMAP2': 'UMAP2'},
-                     color_continuous_scale='Set1')  
+    fig = px.scatter(
+        df,
+        x="UMAP1",
+        y="UMAP2",
+        color="label",
+        title=f"UMAP projection of {args.embedding} features embedding of ProtGPS training sequences",
+        labels={"UMAP1": "UMAP1", "UMAP2": "UMAP2"},
+        color_continuous_scale="Set1",
+    )
     fig.show()
 
 
